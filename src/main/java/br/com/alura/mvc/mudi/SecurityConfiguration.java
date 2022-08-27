93ff38e65efb660e27519943f25b62de8f23cd73
willianguerino <willianguerino@outlook.com>
2022-08-27 18:45:58 -0400

    primeiro commit

---
+++ src/main/java/br/com/alura/mvc/mudi/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+	/**
+	 * Realiza a configuracao da cadeia de seguranca
+	 * @param http
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((authz) -> authz
+			.anyRequest().authenticated()
+			)
+			.formLogin(form -> form
+					.loginPage("/login")
+					.permitAll());
+		return http.build();
+	}
+
+}
