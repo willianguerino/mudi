a17e20e86fb06fc94b1ffb0eafec0b58c4fb68f3
willianguerino <willianguerino@outlook.com>
2022-08-28 12:17:25 -0400

    Problemas na autenticacao.

--- src/main/java/br/com/alura/mvc/mudi/SecurityConfiguration.java
+++ src/main/java/br/com/alura/mvc/mudi/SecurityConfiguration.java
@@ -1,29 +1,55 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
+	@Autowired
+	private DataSource dataSource;
+
 	/**
 	 * Realiza a configuracao da cadeia de seguranca
+	 * 
 	 * @param http
 	 * @return
 	 * @throws Exception
 	 */
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests((authz) -> authz
-			.anyRequest().authenticated()
-			)
-			.formLogin(form -> form
-					.loginPage("/login")
-					.permitAll());
+		http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+				.formLogin(form -> form.loginPage("/login").permitAll())
+				.logout(logout -> logout.logoutSuccessUrl("/logout"));
 		return http.build();
 	}
+	
+	/**
+	 * Prove autenticacao
+	 * @param authenticationConfiguration
+	 * @return
+	 * @throws Exception
+	 */
+	 @Bean
+     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+         return authenticationConfiguration.getAuthenticationManager();
+     }
 
+	/**
+	 * 
+	 * @return
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -2,8 +2,6 @@ package br.com.alura.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
