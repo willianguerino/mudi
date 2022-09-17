a7afc39079f62c6fa763c0130616a46904b949cb
willianguerino <willianguerino@outlook.com>
2022-09-17 10:05:39 -0400

    O pedidos rest esta retornando os pedidos de forma recursiva. preciso corrigir

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,12 +14,13 @@ import br.com.alura.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-	List<Pedido> findByStatus(StatusPedido status);
+	@Cacheable("pedidos")
+	List<Pedido> findByStatus(StatusPedido status, PageRequest paginacao);
+	
 	@Query("select p from Pedido p join p.user u where u.username = :username")
 	List<Pedido> findAllByUsuario(@Param("username") String string);
+	
 	@Query("select p from Pedido p join p.user u where u.username = :username and p.status = :status")
 	List<Pedido> findByStatusAndUser(@Param("status") StatusPedido status,@Param("username") String username);
-
 
 }
